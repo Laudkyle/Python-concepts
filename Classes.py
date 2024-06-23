f1ee407b308b57b23ed16d99b45653251bf3f4d8
laudkyle <kyleaby1@gmail.com>
2024-06-23 18:47:48 +0000

    updating functions

--- Classes.py
+++ Classes.py
@@ -10,3 +10,11 @@ class Person:
     def changeName(self,new_name):
         self.name = new_name
         return "Name changed to {}".format(new_name)
+    
+    def changeAge(self,new_age):
+        self.name = new_age
+        return "Age changed to {}".format(new_age)
+    
+    def changeGender(self,new_gender):
+        self.name = new_gender
+        return "Gender changed to {}".format(new_gender)
