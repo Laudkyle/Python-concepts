7525bfc75ab465f03efd7f7152a172f2f475527d
laudkyle <kyleaby1@gmail.com>
2024-06-24 11:42:39 +0000

    printing the singly linked list

--- singly linked list.py
+++ singly linked list.py
@@ -18,6 +18,14 @@ class LinkedList:
                 else:
                     last_node = last_node.next
             last_node.next = new_node
+            
+    def printList(self):
+        current_node = self.head
+        while True:
+            if current_node is None:
+                break
+            print(current_node.data)
+            current_node = current_node.next
         
 Node1 = Node("node 1")
 list1 = LinkedList(Node1)
