dc50c388bc820f2f7edfcc43c778fc77859f3b5e
laudkyle <kyleaby1@gmail.com>
2024-06-24 11:11:48 +0000

    creating the node

---
+++ singly linked list.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def insert(self, new_node):
+        if self.head is None:
+            self.head = new_node
+        else:
+            last_node = self.head
+            while True:
+                if last_node.next is None:
+                    break
+                else:
+                    last_node = last_node.next
+            last_node.next = new_node
+        
+Node1 = Node("node 1")
+list1 = LinkedList(Node1)
