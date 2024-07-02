4a3cf335a3a2c256bbbd141de8a7c932fd1f5547
laudkyle <kyleaby1@gmail.com>
2024-07-02 08:50:44 +0000

    Done witrh insert at end

--- singly.py
+++ singly.py
@@ -1,34 +1,39 @@
 class Node:
     def __init__(self,data):
         self.data = data
-        self.next= None
+        self.next = None
         
         
 class LinkedList:
-    def __init__(self,head):
-        self.head = head
+    def __init__(self):
+        self.head = None
     
     def insertEnd(self, new_node):
         if self.head is None:
             self.head = new_node
         else:
-            current_node = self.next
+            current_node = self.head
             while True:
-                if current_node is None:
+                if current_node.next is None:
                     break
                 else:
                     current_node = current_node.next
-            current_node = new_node
+            current_node.next = new_node
         
     def show(self):
         current_node = self.head
-        while current_node.next is not None:
-            print(current_node.data)
+        while True:
+            if current_node is not None:
+                print(current_node.data)
+                current_node =current_node.next
+            else:
+                break
             
  
 Node_one = Node("One")
 Node_two = Node("two")
 Node_three = Node("three")
+Node_four = Node("four")
 
 
 
@@ -37,6 +42,7 @@ list_one = LinkedList()
 list_one.insertEnd(Node_one)
 list_one.insertEnd(Node_two)
 list_one.insertEnd(Node_three)
+list_one.insertEnd(Node_four)
 
 list_one.show()
 
