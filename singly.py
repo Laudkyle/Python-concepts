4d3c35760689b42525a1c215db67c736383e0b0a
laudkyle <kyleaby1@gmail.com>
2024-07-02 08:31:35 +0000

    creating the list

--- singly.py
+++ singly.py
@@ -1,8 +1,13 @@
 class Node:
-    def __init__(self,head):
-        self.head = head
+    def __init__(self,data):
+        self.data = data
         self.next= None
         
+        
+class LinkedList:
+    def __init__(self,head):
+        self.head = head
+    
     def insertEnd(self, new_node):
         if self.head is None:
             self.head = new_node
@@ -13,16 +18,18 @@ class Node:
                     break
                 else:
                     current_node = current_node.next
-            current_node =new_node
+            current_node = new_node
         
-        
-        
-class LinkedList:
-    def __init__(self,data):
-        self.data = data
  
- 
-Node_one = Node()
-Node_one.insertEnd("One")        
+Node_one = Node("One")
+Node_two = Node("two")
+Node_three = Node("three")
+
+
+
 list_one = LinkedList()
+
+list_one.insertEnd(Node_one)
+list_one.insertEnd(Node_two)
+list_one.insertEnd(Node_three)
 
